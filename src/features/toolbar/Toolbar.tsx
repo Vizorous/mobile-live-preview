8a999e6c2a5bd1d44fb08df5c3d8463259e0d436
Vizorous <vikumwijekoon97@gmail.com>
2021-08-25 18:05:19 +0530

    MLP-3/feat: added custom toolbar.

---
+++ src/features/toolbar/Toolbar.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import CustomRedo from "./CustomRedo";
+import CustomUndo from "./CustomUndo";
+interface ToolbarProps {}
+
+const Toolbar: React.FC<ToolbarProps> = (props) => {
+  return (
+    <>
+      <div id="toolbar-container">
+        <span className="ql-formats">
+          <select className="ql-font"></select>
+          <select className="ql-size"></select>
+        </span>
+        <span className="ql-formats">
+          <button className="ql-bold"></button>
+          <button className="ql-italic"></button>
+          <button className="ql-underline"></button>
+          <button className="ql-strike"></button>
+        </span>
+        <span className="ql-formats">
+          <select className="ql-color"></select>
+          <select className="ql-background"></select>
+        </span>
+        <span className="ql-formats">
+          <button className="ql-script" value="sub"></button>
+          <button className="ql-script" value="super"></button>
+        </span>
+        <span className="ql-formats">
+          <button className="ql-header" value="1"></button>
+          <button className="ql-header" value="2"></button>
+          <button className="ql-blockquote"></button>
+          <button className="ql-code-block"></button>
+        </span>
+        <span className="ql-formats">
+          <button className="ql-list" value="ordered"></button>
+          <button className="ql-list" value="bullet"></button>
+          <button className="ql-indent" value="-1"></button>
+          <button className="ql-indent" value="+1"></button>
+        </span>
+        <span className="ql-formats">
+          <button className="ql-direction" value="rtl"></button>
+          <select className="ql-align"></select>
+        </span>
+        <span className="ql-formats">
+          <button className="ql-link"></button>
+          <button className="ql-image"></button>
+          <button className="ql-video"></button>
+          <button className="ql-formula"></button>
+        </span>
+        <span className="ql-formats">
+          <button className="ql-clean"></button>
+        </span>
+
+        <span className="ql-formats">
+          <button className="ql-undo">
+            <CustomUndo />
+          </button>
+          <button className="ql-redo">
+            <CustomRedo />
+          </button>
+        </span>
+      </div>
+    </>
+  );
+};
+
+function undoChange() {
+  //@ts-ignore
+  this.quill.history.undo();
+}
+function redoChange() {
+  //@ts-ignore
+  this.quill.history.redo();
+}
+
+export default React.memo(Toolbar);
+export const modules = {
+  toolbar: {
+    container: "#toolbar-container",
+    handlers: {
+      undo: undoChange,
+      redo: redoChange,
+    },
+  },
+  history: {
+    delay: 500,
+    maxStack: 100,
+    userOnly: true,
+  },
+};
