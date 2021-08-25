66bb0d8794645d02dcb830b341a0201339606c06
Vizorous <vikumwijekoon97@gmail.com>
2021-08-25 18:02:21 +0530

    MLP-3/feat: added React Quill.

--- src/features/desktop/DesktopContainer.tsx
+++ src/features/desktop/DesktopContainer.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
 import MobileContainer from "../mobile/MobileContainer";
+import "react-quill/dist/quill.snow.css";
+import Toolbar, { modules } from "../toolbar/Toolbar";
 
 interface DesktopContainerProps {}
 
 const DesktopContainer: React.FC<DesktopContainerProps> = (props) => {
+  const [value, setValue] = useState("");
+
   return (
     <>
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        modules={modules}
+      />
+
       <MobileContainer withFrame={true}></MobileContainer>
     </>
   );
